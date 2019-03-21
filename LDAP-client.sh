be31eda2cbef9b8fdf77f60687c3ae843fca6bf9
jplittle <43659489+jplittle@users.noreply.github.com>
2019-03-21 10:20:18 -0700

    Update LDAP-client.sh

--- LDAP-client.sh
+++ LDAP-client.sh
@@ -50,12 +50,14 @@ chown 600 /etc/ldap.secret
 sudo auth-client-config -t nss -p lac_ldap
 # makes it so that we don't have to use a password to su to differen users
 echo "account sufficient pam_succeed_if.so uid = 0 use_uid quiet" >> /etc/pam.d/su
+
+sed -i 's/base dc=example,dc=net/base dc=nti310,dc=local/g'
+sed -i 's, ,uri ldapi:///,uri ldap://ldap2/,g' /etc/ldap.conf 
+sed -i 's/rootbindn cn=manager,dc=example,dc=net/rootbinddn cn=ldapadm,dc=nti310,dc=local/g' /etc/dap.conf 
+
 # restarting nscd, enabling nscd
 systemctl restart nscd
 systemctl enable nscd
 
 
 
-#sed -i 's/base dc=example,dc=net/base dc=nti310,dc=local/g'
-#sed -i 's, ,uri ldapi:///,uri ldap://ldap2/,g' /etc/ldap.conf 
-#sed -i 's/rootbindn cn=manager,dc=example,dc=net/rootbinddn cn=ldapadm,dc=nti310,dc=local/g' /etc/dap.conf 
